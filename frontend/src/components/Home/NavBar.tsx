1edb951921745a56fbdbbf0ed8198bfc0af9903a
Mena Attia <menaattia@users.noreply.github.com>
2020-11-07 02:21:13 -0500

    replace homepage collapsible menu with dropdown menu

--- frontend/src/components/Home/NavBar.tsx
+++ frontend/src/components/Home/NavBar.tsx
@@ -1,14 +1,62 @@
 import React, { ReactElement } from 'react';
 import icon from '../images/home-icon.png'
 import { Link } from 'react-router-dom'
+import HamburgerMenu from 'react-hamburger-menu'
 
 const NavBar= (): ReactElement => {
   const [toggleMenu, setToggle] = React.useState(false)
   const clickToggle = (): void => {
     setToggle(!toggleMenu)
   }
+  const [width, setWidth] = React.useState(0)
+  const update = (): void => {
+    setWidth(window.innerWidth)
+    if (width > 1200) {
+      setToggle(false)
+    }
+  }
 
-  const show: string = toggleMenu? "show" : "";
+  window.addEventListener('resize', update)
+  React.useEffect(() => {
+    update()
+  }, [])
+
+
+  const displayHamburgerMenu = (): ReactElement => {
+    return (
+      <HamburgerMenu
+                    isOpen={toggleMenu}
+                    menuClicked={clickToggle}
+                    width={25}
+                    height={20}
+                    strokeWidth={1}
+                    rotate={0}
+                    color='black'
+                    borderRadius={0}
+                    animationDuration={0.5}
+                />
+    )
+  }
+
+  const displayMobileMenu = (): ReactElement => {
+    return (
+      <ul className='hamburgerDropDown'>
+              <li><a className="nav-link" href="/faq">FAQ</a></li>
+      </ul>
+  )
+  }
+
+  const displayNavBar = (): ReactElement => {
+    return (
+      <ul className='nav nav-button-margin' >
+              <li>
+                <button type="button" className="btn btn-lg btn-outline-dark" > 
+                <Link to='/faq' className="links">FAQ</Link>
+                </button>
+              </li>
+      </ul>
+  )
+  }
 
   return (
     <div>
@@ -16,18 +64,11 @@ const NavBar= (): ReactElement => {
       <a className="navbar-brand" href="/">
       <h1><img className="logo" src={icon} width="40" height="auto" alt="home icon"/> CU Housing</h1>
       </a>
-      <button className="navbar-toggler collapsed" onClick={clickToggle} type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className={"collapse navbar-collapse "+ show} id="navbarNav">
-        <ul className="navbar-nav ml-auto w-100 justify-content-end">
-          <li className="nav-item active">
-            {toggleMenu? <a className="nav-link" href="/faq">FAQ</a> : 
-              <button type="button" className="btn btn-lg btn-outline-dark"><Link to='/faq' className="links">FAQ</Link></button> }
-          </li>
-        </ul>
-      </div>  
+      { toggleMenu ?  displayMobileMenu() : null}
+      {width > 1200 ? displayNavBar() : displayHamburgerMenu()}
+      
     </nav>
+    
     <div className="homepage-description">
     <h5>Search for off-campus housing, review apartments, and share feedback!</h5>
   </div>
