def505f481a211016fa13e7687c1b60f596bb32e
Sam Liu <sl2486@cornell.edu>
2021-02-21 16:35:49 -0500

    FAQPage spinner, css changes for button links

---
+++ frontend/src/components/FAQ/FAQSpinner.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+import styles from './FAQSpinner.module.scss';
+
+export default function FAQSpinner(): ReactElement {
+  return (
+    <div className={styles.container}> 
+      <Spinner animation="border" className={styles.spinner} />
+    </div>
+  )
+}
