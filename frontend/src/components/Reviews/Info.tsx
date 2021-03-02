229edc8dca2ffcf2e0dc20f3184ec8ca5f3d8488
Sam Liu <sl2486@cornell.edu>
2021-03-02 17:45:59 -0500

    Initial implementation of info/features
    - Info/Feature components appear on the right side
    - No support for mobile resizing yet

---
+++ frontend/src/components/Reviews/Info.tsx
@@ -0,0 +1,24 @@
+import React, { ReactElement } from 'react';
+import Grid from '@material-ui/core/Grid';
+import styles from './Reviews.module.scss';
+
+type Props = {
+  readonly image: string,
+  readonly phone: string,
+  readonly address: string
+};
+
+export default function Info({ image, phone, address }: Props): ReactElement {
+  return (
+    <div className={styles.component}>
+
+        <p className={styles.title}>Info</p>
+        <div className={styles.detail}>
+          <p>{phone}</p>
+          <p>{address}</p>
+          <img className={styles.image} src={image} alt="" />
+        </div>
+
+    </div>
+  )
+}
