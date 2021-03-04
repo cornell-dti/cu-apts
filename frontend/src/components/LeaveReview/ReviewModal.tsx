6eaaa84d8d37a751612a70bc0208d4f0b0f93a75
Enochen <darakath1128@gmail.com>
2021-03-04 18:59:29 -0500

    add scrollbar

--- frontend/src/components/LeaveReview/ReviewModal.tsx
+++ frontend/src/components/LeaveReview/ReviewModal.tsx
@@ -100,73 +100,76 @@ const ReviewModal = ({ open, onClose }: Props) => {
   return (
     <Dialog open={open} onClose={onClose} fullWidth maxWidth="md">
       <DialogTitle>Leave a Review</DialogTitle>
-      <DialogContent style={{ overflow: 'hidden' }}>
-        <Grid container direction="column" justify="space-evenly" spacing={4}>
-          <Grid container item alignContent="center">
-            <Grid container item justify="space-between" xs={12} sm={6}>
-              <TextField
-                fullWidth
-                autoFocus
-                label="Name"
-                value={review.name}
-                onChange={updateName}
-              />
-            </Grid>
-            <Grid container justify="center" item xs={12} sm={6}>
-              <Grid item>
-                <FormControlLabel
-                  control={<Switch onChange={updateAnonymous} />}
-                  label="Review anonymously"
+      <DialogContent>
+        {/* This div padding prevents the scrollbar from displaying unnecessarily */}
+        <div style={{ padding: 8 }}>
+          <Grid container direction="column" justify="space-evenly" spacing={4}>
+            <Grid container item alignContent="center">
+              <Grid container item justify="space-between" xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  autoFocus
+                  label="Name"
+                  value={review.name}
+                  onChange={updateName}
                 />
               </Grid>
+              <Grid container justify="center" item xs={12} sm={6}>
+                <Grid item>
+                  <FormControlLabel
+                    control={<Switch onChange={updateAnonymous} />}
+                    label="Review anonymously"
+                  />
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid container item>
+              <Grid container justify="center">
+                <ReviewRating
+                  name="management"
+                  label="Management/Landlord"
+                  onChange={updateRating('management')}
+                ></ReviewRating>
+                <ReviewRating
+                  name="maintenence"
+                  label="Building Maintenence"
+                  onChange={updateRating('maintenence')}
+                ></ReviewRating>
+                <ReviewRating
+                  name="amenities"
+                  label="Building Amenities"
+                  onChange={updateRating('amenities')}
+                ></ReviewRating>
+                <ReviewRating
+                  name="condition"
+                  label="Building Condition"
+                  onChange={updateRating('condition')}
+                ></ReviewRating>
+                <ReviewRating
+                  name="neighborhood"
+                  label="Neighborhood & Neighbors"
+                  onChange={updateRating('neighborhood')}
+                ></ReviewRating>
+                <ReviewRating
+                  name="transportation"
+                  label="Transportation and Parking"
+                  onChange={updateRating('transportation')}
+                ></ReviewRating>
+              </Grid>
+            </Grid>
+            <Grid container item>
+              <TextField
+                fullWidth
+                id="standard-multiline-static"
+                label="Review"
+                multiline
+                rows={6}
+                placeholder="Write your review here"
+                onChange={updateBody}
+              />
             </Grid>
           </Grid>
-          <Grid container item>
-            <Grid container justify="center">
-              <ReviewRating
-                name="management"
-                label="Management/Landlord"
-                onChange={updateRating('management')}
-              ></ReviewRating>
-              <ReviewRating
-                name="maintenence"
-                label="Building Maintenence"
-                onChange={updateRating('maintenence')}
-              ></ReviewRating>
-              <ReviewRating
-                name="amenities"
-                label="Building Amenities"
-                onChange={updateRating('amenities')}
-              ></ReviewRating>
-              <ReviewRating
-                name="condition"
-                label="Building Condition"
-                onChange={updateRating('condition')}
-              ></ReviewRating>
-              <ReviewRating
-                name="neighborhood"
-                label="Neighborhood & Neighbors"
-                onChange={updateRating('neighborhood')}
-              ></ReviewRating>
-              <ReviewRating
-                name="transportation"
-                label="Transportation and Parking"
-                onChange={updateRating('transportation')}
-              ></ReviewRating>
-            </Grid>
-          </Grid>
-          <Grid container item>
-            <TextField
-              fullWidth
-              id="standard-multiline-static"
-              label="Review"
-              multiline
-              rows={6}
-              placeholder="Write your review here"
-              onChange={updateBody}
-            />
-          </Grid>
-        </Grid>
+        </div>
       </DialogContent>
       <DialogActions>
         <Button onClick={onSubmit}>Submit</Button>
