2cd43da472df50791b9ac71ab52f0eb90168090e
Mena Attia <mena.attia.ma@gmail.com>
2021-03-06 19:08:29 -0500

    Migrate Review component to Material-UI

--- frontend/src/components/Review/index.tsx
+++ frontend/src/components/Review/index.tsx
@@ -1,6 +1,8 @@
 import React, { ReactElement } from 'react';
-import styles from './Review.module.scss';
-import Card from 'react-bootstrap/Card';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 import Rating from './Rating';
 import dateToString from '../../utils/date';
 
@@ -10,18 +12,44 @@ type Props = {
   readonly text: string;
 };
 
+const useStyles = makeStyles({
+  root: {
+    width: '60vw',
+    '@media (max-width: 650px)': {
+      width: '94vw',
+    },
+    borderRadius: '10px',
+    marginLeft: '3vw',
+    marginRight: '3vw',
+    marginBottom: '3vh',
+    boxShadow: 'none',
+  },
+  title: {
+    display: 'inline-block',
+    marginRight: '10px',
+  },
+  dateText: {
+    '@media (min-width: 650px)': {
+      position: 'relative',
+      float: 'right',
+      paddingTop: '8px'
+    }
+  },
+});
+
 const Review = ({ overallRating, date, text }: Props): ReactElement => {
+  const {root, title, dateText} = useStyles();
   return (
-    <Card className={styles.card}>
-      <Card.Body>
-        <Card.Title>Anonymous </Card.Title>
+    <Card variant="outlined" className={root}>
+      <CardContent>
+        <Typography className={title} variant="h5" component="h2">Anonymous </Typography>
         <Rating rating={overallRating} />
-        <Card.Subtitle className={`${styles.date} mb-2 text-muted`}>
+        <Typography className={`${dateText} mb-2 text-muted`}>
           {dateToString(date)}
-        </Card.Subtitle>
+        </Typography>
         <hr />
-        <Card.Text>{text}</Card.Text>
-      </Card.Body>
+        <Typography variant="body2" component="p">{text}</Typography>
+      </CardContent>
     </Card>
   );
 };
