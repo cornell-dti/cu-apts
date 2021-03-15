cb7e9e6858cc89783a9c244a7d06158acbad0b83
Mena Attia <mena.attia.ma@gmail.com>
2021-03-14 20:32:39 -0400

    run prettier

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
-import app from './app'
+import app from './app';
 
-const port = process.env.PORT || 8080; 
+const port = process.env.PORT || 8080;
 
 // eslint-disable-next-line
 app.listen(port, () => console.log(`Server running on port: ${port}`));
