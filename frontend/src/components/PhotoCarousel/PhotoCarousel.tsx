abc349aa41d1837e062aaa708a63b46f4f785a4d
Enochen <darakath1128@gmail.com>
2021-03-24 18:27:22 -0400

    add carousel and btns

---
+++ frontend/src/components/PhotoCarousel/PhotoCarousel.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Modal, Box, styled, Container, CardMedia } from '@material-ui/core';
+import Carousel from 'react-material-ui-carousel';
+
+interface Props {
+  photos: string[];
+  open: boolean;
+  onClose?: () => void;
+}
+
+const CenteredModal = styled(Modal)({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+});
+
+const ImageBox = styled(Box)({
+  width: 'fit-content',
+  margin: 'auto',
+});
+
+const PhotoCarousel = ({ photos, open, onClose }: Props) => (
+  <CenteredModal open={open} onClose={onClose} disableRestoreFocus>
+    <Container>
+      <Carousel autoPlay={false}>
+        {photos.map((src) => {
+          return (
+            <ImageBox>
+              <CardMedia component="img" src={src} />
+            </ImageBox>
+          );
+        })}
+      </Carousel>
+    </Container>
+  </CenteredModal>
+);
+
+export default PhotoCarousel;
--- frontend/src/pages/LandlordPage.tsx
+++ frontend/src/pages/LandlordPage.tsx
@@ -1,6 +1,8 @@
-import { Container, Grid } from '@material-ui/core';
+import { Button, Container, Grid } from '@material-ui/core';
 import React, { ReactElement, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import ReviewModal from '../components/LeaveReview/ReviewModal';
+import PhotoCarousel from '../components/PhotoCarousel/PhotoCarousel';
 import InfoFeatures from '../components/Review/InfoFeatures';
 import Review from '../components/Review/Review';
 import { getWidth } from '../utils/isMobile';
@@ -8,6 +10,7 @@ import { getWidth } from '../utils/isMobile';
 type LandlordData = {
   features: string[];
   properties: string[];
+  photos: string[];
   phone: string;
   address: string;
 };
@@ -20,7 +23,6 @@ const reviews = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam auctor mauris a scelerisque rhoncus. Nam vitae lacus at neque faucibus porttitor. Phasellus mollis maximus neque, vehicula consectetur enim sagittis ac. Sed viverra risus nibh, non pulvinar mauris fermentum sed. Praesent pellentesque dapibus felis nec interdum. ',
   },
   {
-    name: 'user2',
     overallRating: 2,
     date: new Date(),
     text:
@@ -37,6 +39,10 @@ const reviews = [
 const dummyData: LandlordData = {
   properties: ['111 Dryden Rd', '151 Dryden Rd', '418 Eddy St'],
   features: ['Parking', 'Heating', 'Trash removal', 'Snow plowing', 'Maintenance'],
+  photos: [
+    'https://lifestylepropertiesithaca.com/gridmedia/img/slide1.jpg',
+    'https://images1.apartments.com/i2/F7HtEfdZCVtvQ_DcqGjQuoQ2IcmcMb2nP1PJuOwOdFw/102/carriage-house-apartments-ithaca-ny-primary-photo.jpg',
+  ],
   phone: '555-555-5555',
   address: '119 S Cayuga St, Ithaca, NY 14850',
 };
@@ -47,57 +53,61 @@ const LandlordPage = (): ReactElement => {
   const [landlordData] = useState(dummyData);
   const breakpoint = 600;
 
+  const [reviewOpen, setReviewOpen] = useState(false);
+  const [carouselOpen, setCarouselOpen] = useState(false);
+
   useEffect(() => {
     window.addEventListener('resize', () => setWidth(getWidth()));
   });
 
+  const Modals = (
+    <>
+      <ReviewModal open={reviewOpen} onClose={() => setReviewOpen(false)} />
+      <PhotoCarousel
+        photos={landlordData.photos}
+        open={carouselOpen}
+        onClose={() => setCarouselOpen(false)}
+      />
+    </>
+  );
+
   return (
-    <Container>
-      <h1>{`This is dummy text! My current landlordId is ${landlordId}`}</h1>
+    <>
       <Container>
-        <Grid container spacing={3}>
-          {width >= breakpoint ? (
-            <>
-              <Grid item xs={12} sm={8}>
-                <Grid container spacing={3}>
-                  {reviews.map((reviewData, index) => (
-                    <Grid item xs={12}>
-                      <Review {...reviewData} key={index} />
-                    </Grid>
-                  ))}
-                </Grid>
+        <h1>{`This is dummy text! My current landlordId is ${landlordId}`}</h1>
+        <Container>
+          <Button variant="contained" disableElevation onClick={() => setCarouselOpen(true)}>
+            Show all photos
+          </Button>
+          <Button
+            color="primary"
+            variant="contained"
+            disableElevation
+            onClick={() => setReviewOpen(true)}
+          >
+            Leave a Review
+          </Button>
+          <Grid container spacing={3} direction={width >= breakpoint ? 'row-reverse' : 'column'}>
+            <InfoFeatures
+              propertyInfo={landlordData.properties}
+              propertyFeatures={landlordData.features}
+              phone={landlordData.phone}
+              address={landlordData.address}
+            />
+            <Grid item xs={12} sm={8}>
+              <Grid container spacing={3}>
+                {reviews.map((reviewData, index) => (
+                  <Grid item xs={12}>
+                    <Review {...reviewData} key={index} />
+                  </Grid>
+                ))}
               </Grid>
-
-              <InfoFeatures
-                propertyInfo={landlordData.properties}
-                propertyFeatures={landlordData.features}
-                phone={landlordData.phone}
-                address={landlordData.address}
-              />
-            </>
-          ) : (
-            <>
-              <InfoFeatures
-                propertyInfo={landlordData.properties}
-                propertyFeatures={landlordData.features}
-                phone={landlordData.phone}
-                address={landlordData.address}
-              />
-
-              <Grid item xs={12}>
-                <Grid container spacing={3}>
-                  {reviews.map((reviewData, index) => (
-                    <Grid item xs={12}>
-                      <Review {...reviewData} key={index} />
-                    </Grid>
-                  ))}
-                </Grid>
-              </Grid>
-            </>
-          )}
-        </Grid>
+            </Grid>
+          </Grid>
+        </Container>
       </Container>
-    </Container>
+      {Modals}
+    </>
   );
 };
 
