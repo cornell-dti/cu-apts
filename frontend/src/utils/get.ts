e5398551278a7e01db9a9ed80d2131d2a4f7e0c6
Mena Attia <31854132+menaattia@users.noreply.github.com>
2021-04-04 02:50:16 -0400

    Get Reviews from Backend (#50)
    * Set up getting reviews from backend
    
    * Create function to make get requests to backend
    
    * Change types to match Review type in dt-types
    
    * Update url in reviews get request
    
    * Convert firestore Timestamp to Date type in get request, remove dummy review data
    
    * Factor out url in get function
    
    * Fix linter error
    
    * Add @firebase/testing dependency

---
+++ frontend/src/utils/get.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const backendUrl = 'http://localhost:8080';
+
+export default function get<T>(route: string, setState: (data: T[]) => void) {
+  axios
+    .get(`${backendUrl}${route}`)
+    .then((response) => {
+      setState(response.data);
+    })
+    .catch((error) => {
+      console.log('error', error);
+    });
+}
