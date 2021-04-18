3292542f109abcdc9dca741eb9bc95daba622ed2
Sam Liu <sl2486@cornell.edu>
2021-04-18 15:11:32 -0400

    Script to add reviews to db

--- backend/src/script.ts
+++ backend/src/script.ts
@@ -1,37 +1,52 @@
 import express, { Express } from 'express';
 import cors from 'cors';
-import axios from 'axios';
-import CSVToJSON from 'csvtojson';
-// import { db } from './firebase-config';
+import { db } from './firebase-config';
+import reviewData from './landlord_reviews.json';
 import { Review } from '../../common/types/db-types';
 
-const FILE = './landlord_reviews.csv';
+const reviewCollection = db.collection('reviews');
+
 const app: Express = express();
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:8080',
   })
 );
 
 const makeReview = async (review: Review) => {
-  axios
-    .post('/new-review', review)
+  try {
+    const doc = reviewCollection.doc();
+    doc.set({ ...review, date: new Date(review.date) });
+  } catch (err) {
     // eslint-disable-next-line no-console
-    .catch((err) => console.log(err));
+    console.log(err);
+  }
 };
 
-CSVToJSON()
-  .fromFile(FILE)
-  .then(
-    (json) => {
-      const reviews = json as Review[];
-      // eslint-disable-next-line no-console
-      console.log('hi');
-      reviews.map((review) => makeReview(review));
-    },
-    // eslint-disable-next-line no-console
-    (error) => console.log(error)
-  );
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const formatReview = (data: any): Review => ({
+  landlordId: data.landlordId.toString(),
+  aptId: data.aptId.toString(),
+  reviewText: data.reviewText,
+  overallRating: data.overallRating,
+  detailedRatings: {
+    location: data['detailedRatings.location'],
+    safety: data['detailedRatings.safety'],
+    value: data['detailedRatings.value'],
+    maintenance: data['detailedRatings.maintenance'],
+    communication: data['detailedRatings.communication'],
+    conditions: data['detailedRatings.condition'],
+  },
+  date: data.date,
+});
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const modifyData = async (data: any[]) => {
+  const reviews = data.map((review) => formatReview(review));
+  reviews.map((review) => makeReview(review));
+};
+
+modifyData(reviewData);
 
 const port = process.env.PORT || 8080;
 
