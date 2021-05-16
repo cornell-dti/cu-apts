3aa28c17ec446335b7247fc9653b3ec6016364fb
Sam Liu <sl2486@cornell.edu>
2021-05-16 16:23:49 -0400

    batch script to write landlords to db

--- backend/src/script.ts
+++ backend/src/script.ts
@@ -1,8 +1,10 @@
 import { db } from './firebase-config';
-import reviewData from './landlord_reviews.json';
-import { Review } from '../../common/types/db-types';
+import reviewData from './data/landlord_reviews.json';
+import landlordData from './data/landlords.json';
+import { Landlord, LandlordWithId, Review } from '../../common/types/db-types';
 
 const reviewCollection = db.collection('reviews');
+const landlordCollection = db.collection('landlords');
 
 const makeReview = async (review: Review) => {
   try {
@@ -32,4 +34,38 @@ const formatReview = (data: any): Review => ({
   photos: data.photos,
 });
 
+const formatLandlord = ({
+  id,
+  name,
+  contact,
+  avgRating,
+  photos,
+  reviews,
+  properties,
+}: // eslint-disable-next-line @typescript-eslint/no-explicit-any
+any): LandlordWithId => ({
+  id: id.toString(),
+  name,
+  contact,
+  avgRating: avgRating.length > 0 ? Number.parseFloat(avgRating) : 0,
+  photos: photos.length > 0 ? photos : [],
+  reviews: reviews.length > 0 ? reviews : [],
+  properties: properties.length > 0 ? properties : [],
+});
+
+const makeLandlord = async (landlordWithId: LandlordWithId) => {
+  try {
+    const doc = landlordCollection.doc(landlordWithId.id);
+    const landlord = landlordWithId as Landlord;
+
+    console.log(`landlord = ${JSON.stringify({ ...landlord })}`);
+    doc.set({ ...landlord });
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+  }
+};
+
 reviewData.map((review) => makeReview(formatReview(review)));
+
+landlordData.map((landlord) => makeLandlord(formatLandlord(landlord)));
