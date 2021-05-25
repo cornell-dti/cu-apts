f4b5c7c2c988523c393911757a36d86459118e79
Enochen <darakath1128@gmail.com>
2021-05-25 13:07:37 -0400

    update design

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,8 +1,18 @@
-import { Container } from '@material-ui/core';
+import { Box, Container, Typography } from '@material-ui/core';
 import React, { ReactElement } from 'react';
+import NotFoundIcon from '../assets/not-found.svg';
 
 const NotFoundPage = (): ReactElement => {
-  return <Container>404</Container>;
+  return (
+    <Container maxWidth="md">
+      <Box pb={3} textAlign="center">
+        <img src={NotFoundIcon} alt="CU Apts Logo" />
+        <Typography variant="h5">
+          It seems as though the page you are looking for cannot be found!
+        </Typography>
+      </Box>
+    </Container>
+  );
 };
 
 export default NotFoundPage;
