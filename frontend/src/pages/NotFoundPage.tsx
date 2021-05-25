448f54b967a6c8632fb8fa1bd668ac685f08c60a
Peter Wu <plw53@cornell.edu>
2021-05-25 14:00:36 -0400

    Fix 404 page on mobile viewports

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,12 +1,13 @@
 import { Box, Container, Typography } from '@material-ui/core';
 import React, { ReactElement } from 'react';
 import NotFoundIcon from '../assets/not-found.svg';
+import styles from './NotFoundPage.module.css';
 
 const NotFoundPage = (): ReactElement => {
   return (
     <Container maxWidth="md">
       <Box pb={3} textAlign="center">
-        <img src={NotFoundIcon} alt="CU Apts Logo" />
+        <img src={NotFoundIcon} alt="CU Apts Logo" className={styles.NotFoundImage} />
         <Typography variant="h5">
           It seems as though the page you are looking for cannot be found!
         </Typography>
