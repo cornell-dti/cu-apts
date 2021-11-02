a7db151684cd74ea0282ec538191c07035fcdeca
Kinjal Jasani <kjasani111@gmail.com>
2021-11-01 20:20:59 -0400

    Reset index.ts to main due to prettier formatting issue

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -8,8 +8,9 @@ import { useEffect } from 'react';
 export const useTitle = (titleOrFn: string | (() => string), ...deps: any[]) => {
   useEffect(
     () => {
-      document.title = ` CU Apartments | ${typeof titleOrFn === 'function' ? titleOrFn() : titleOrFn
-        }`;
+      document.title = ` CU Apartments | ${
+        typeof titleOrFn === 'function' ? titleOrFn() : titleOrFn
+      }`;
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [...deps]
