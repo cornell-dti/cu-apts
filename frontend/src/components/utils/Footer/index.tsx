4f58a786030ec0703f692609864b76bd177a4eb9
Sam Liu <sl2486@cornell.edu>
2021-11-03 20:54:38 -0400

    built with love, minor CSS changes to footer

--- frontend/src/components/utils/Footer/index.tsx
+++ frontend/src/components/utils/Footer/index.tsx
@@ -1,9 +1,12 @@
 import { Grid, Link, makeStyles, Typography } from '@material-ui/core';
 import React, { ReactElement } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
+import Heart from '../../../assets/red-heart.svg';
 
 const useStyles = makeStyles(() => ({
   footer: {
+    marginTop: '8px',
+    marginBottom: '8px',
     backgroundColor: '#F9F9F9',
     height: '94px',
     fontFamily: 'Work Sans',
@@ -24,13 +27,20 @@ const useStyles = makeStyles(() => ({
     textAlign: 'center',
     marginLeft: '4px',
   },
+  heart: {
+    marginLeft: '4px',
+  },
+  item: {
+    margin: '2px',
+    alignItems: 'center',
+  },
 }));
 const Footer = (): ReactElement => {
-  const { footer, contact } = useStyles();
+  const { footer, contact, heart, item } = useStyles();
   return (
     <footer>
-      <Grid container alignItems="center" justify="center" className={footer} direction="column">
-        <Grid item>
+      <Grid container alignItems="center" justify="center" className={footer} direction="row">
+        <Grid item className={item} xs={12}>
           <Grid container alignItems="center" justify="center">
             <Grid item>
               <Typography>Got a question that isn’t answered? </Typography>
@@ -44,16 +54,31 @@ const Footer = (): ReactElement => {
             </Grid>
           </Grid>
         </Grid>
-        <Grid item>
-          <Link
-            {...{
-              to: `/policies`,
-              style: { color: 'black' },
-              component: RouterLink,
-            }}
-          >
-            <Typography> Privacy Policy </Typography>
-          </Link>
+        <Grid item className={item} xs={12}>
+          <Grid container alignItems="center" justify="center">
+            <Grid item>
+              <Link
+                {...{
+                  to: `/policies`,
+                  style: { color: 'black' },
+                  component: RouterLink,
+                }}
+              >
+                <Typography> Privacy Policy </Typography>
+              </Link>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item className={item} xs={12}>
+          <Grid container alignItems="stretch" justify="center">
+            <Grid item>
+              {' '}
+              <Typography> Built With </Typography>
+            </Grid>
+            <Grid item>
+              <img className={heart} src={Heart} alt="red heart" width="22" height="22"></img>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </footer>
