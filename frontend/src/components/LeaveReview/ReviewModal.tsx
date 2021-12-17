7dcd311790699b3718e42f16ca5e4caabe7f8bcd
Sam Liu <57203639+samliu10@users.noreply.github.com>
2021-12-17 17:32:31 -0500

    Sign in immediately after pressing "leave a review" (#111)

--- frontend/src/components/LeaveReview/ReviewModal.tsx
+++ frontend/src/components/LeaveReview/ReviewModal.tsx
@@ -309,13 +309,11 @@ const ReviewModal = ({
                   maxlength: REVIEW_CHARACTER_LIMIT,
                 }}
                 placeholder="Write your review here"
-                helperText={`${review.body.length}/${REVIEW_CHARACTER_LIMIT}${
-                  emptyTextError ? ' This field is required' : ''
-                }${
-                  includesProfanityError
+                helperText={`${review.body.length}/${REVIEW_CHARACTER_LIMIT}${emptyTextError ? ' This field is required' : ''
+                  }${includesProfanityError
                     ? ' This review contains profanity. Please edit it and try again.'
                     : ''
-                }`}
+                  }`}
                 onChange={updateBody}
               />
             </Grid>
