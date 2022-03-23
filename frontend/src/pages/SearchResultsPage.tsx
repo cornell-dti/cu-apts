1354b57ab2569bd87a69485bdbe9ed1fe72e5e04
Jiayuan Gu <jiayuangu762943@gmail.com>
2022-03-22 22:02:58 -0400

    show search query in search-results-page

--- frontend/src/pages/SearchResultsPage.tsx
+++ frontend/src/pages/SearchResultsPage.tsx
@@ -1,15 +1,16 @@
 import { Box, Container, Typography } from '@material-ui/core';
 import React, { ReactElement } from 'react';
+import { useLocation } from 'react-router-dom';
 
 // import NotFoundIcon from '../assets/not-found.svg';
 // import styles from './NotFoundPage.module.css';
 
 const SearchResultsPage = (): ReactElement => {
+  const location = useLocation();
   return (
     <Container maxWidth="md">
       <Box pb={3} textAlign="center">
-        {/* <img src={NotFoundIcon} alt="CU Apts Logo" className={styles.NotFoundImage} /> */}
-        <Typography variant="h5">EMPTY NOW!</Typography>
+        <Typography variant="h5">EMPTY NOW! {location.search.substring(3)}</Typography>
       </Box>
     </Container>
   );
