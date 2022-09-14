863fe83e54b8c1c38ebd972bd5d00a85618b0b4d
Cyrus Irani <cri23@cornell.edu>
2022-09-13 21:10:22 -0400

    took out unused vars

--- frontend/src/pages/LocationPage.tsx
+++ frontend/src/pages/LocationPage.tsx
@@ -1,33 +1,11 @@
 import React, { ReactElement } from 'react';
-import { Box, Container, Typography, makeStyles } from '@material-ui/core';
+import { Box, Container, Typography } from '@material-ui/core';
 import styles from './HomePage.module.scss';
-import { colors } from '../colors';
 import { LocationCardData } from '../App';
 
 type Props = {
   data: LocationCardData[];
 };
-
-const useStyles = makeStyles({
-  jumboText: {
-    color: colors.white,
-    fontWeight: 600,
-    margin: '0.5em 0 0.5em 0',
-  },
-  jumboSub: {
-    color: colors.white,
-    fontWeight: 400,
-  },
-  rentingBox: {
-    marginTop: '2em',
-    marginBottom: '2em',
-  },
-  rentingText: {
-    marginBottom: '1em',
-    fontWeight: 500,
-    color: colors.red1,
-  },
-});
 
 const LocationPage = ({ data }: Props): ReactElement => {
   return (
