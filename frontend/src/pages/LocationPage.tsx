c167e26b749660886f69d99b054562a2817bdf51
Cyrus Irani <60114462+cyrus14@users.noreply.github.com>
2022-10-24 18:18:08 -0400

    LocationPage with 2 Apartment Cards (#199)

--- frontend/src/pages/LocationPage.tsx
+++ frontend/src/pages/LocationPage.tsx
@@ -1,27 +1,24 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState, useEffect } from 'react';
 import { Box, CardMedia, Container, Typography, makeStyles } from '@material-ui/core';
-import { LocationCardData } from '../App';
+import { CardData } from '../App';
 import { useLocation } from 'react-router-dom';
 import CollegetownImg from '../assets/collegetown-coverpic.svg';
 import WestImg from '../assets/west-coverpic.svg';
 import NorthImg from '../assets/north-coverpic.svg';
 import DowntownImg from '../assets/downtown-coverpic.svg';
 import { colors } from '../colors';
+import { get } from '../utils/call';
+import ApartmentCards from '../components/ApartmentCard/ApartmentCards';
 
 interface Images {
   [location: string]: string;
 }
 
-type Props = {
-  data: LocationCardData[];
-};
-
 const useStyles = makeStyles({
   imgStyle: {
-    borderRadius: '25px',
+    borderRadius: '20px',
     height: '300px',
     width: '100%',
-    padding: '10px',
   },
   titleStyle: {
     marginBottom: '40px',
@@ -29,7 +26,7 @@ const useStyles = makeStyles({
     color: colors.white,
     fontSize: '40px',
     marginLeft: '50px',
-    marginTop: '-80px',
+    marginTop: '-70px',
   },
   subtitleStyle: {
     fontWeight: 700,
@@ -40,7 +37,9 @@ const useStyles = makeStyles({
   },
 });
 
-const LocationPage = ({ data }: Props): ReactElement => {
+const LocationPage = (): ReactElement => {
+  const [homeData, setHomeData] = useState<CardData[]>([]);
+
   const path = useLocation();
   const location = path.pathname.substring(path.pathname.lastIndexOf('/') + 1);
   const locToImg: Images = {
@@ -49,6 +48,13 @@ const LocationPage = ({ data }: Props): ReactElement => {
     North: NorthImg,
     Downtown: DowntownImg,
   };
+
+  useEffect(() => {
+    get<CardData[]>(`/location/${location}`, {
+      callback: setHomeData,
+    });
+  }, [location]);
+
   const locDescText: Images = {
     Collegetown:
       'Living in Collegetown allows you to Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -76,6 +82,7 @@ const LocationPage = ({ data }: Props): ReactElement => {
           <Typography variant="body1" className={bodyStyle}>
             {desc}
           </Typography>
+          <ApartmentCards data={homeData} />
         </Container>
       </Box>
     </>
