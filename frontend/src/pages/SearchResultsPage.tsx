6ad3979b5ba0082aafcb5566bf6e7dd4f729f51e
juliejeong <julie.seoyeon.jeong@gmail.com>
2022-11-05 14:24:35 -0400

    removed the old landlord stuff

--- frontend/src/pages/SearchResultsPage.tsx
+++ frontend/src/pages/SearchResultsPage.tsx
@@ -1,10 +1,9 @@
-import { Box, Container, Typography, Grid, makeStyles } from '@material-ui/core';
+import { Container, Typography, makeStyles } from '@material-ui/core';
 import React, { ReactElement, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { get } from '../utils/call';
-import { LandlordOrApartmentWithLabel, LandlordWithLabel } from '../../../common/types/db-types';
-import LandlordCard from '../components/LandlordCard/LandlordCard';
 import { colors } from '../colors';
+import { CardData } from '../App';
 
 const useStyles = makeStyles({
   landlordTitle: {
@@ -18,39 +17,17 @@ const useStyles = makeStyles({
 });
 const SearchResultsPage = (): ReactElement => {
   const location = useLocation();
-  const [searchResults, setSearchResults] = useState<LandlordOrApartmentWithLabel[]>([]);
+  const [searchResults, setSearchResults] = useState<CardData[]>([]);
   const query = location.search.substring(3);
   const { landlordTitle } = useStyles();
 
-  useEffect(() => {
-    get<LandlordOrApartmentWithLabel[]>(`/search?q=${query}`, {
-      callback: (data) => {
-        setSearchResults(data);
-      },
-    });
-  }, [query]);
+  useEffect(() => {}, [query]);
 
-  // if properties is in searchItem, then it has the LandlordWithLabel type
-  function isLandlord(searchItem: LandlordOrApartmentWithLabel): searchItem is LandlordWithLabel {
-    return 'properties' in searchItem;
-  }
-  const landlordSearchResults: LandlordWithLabel[] = searchResults.filter(isLandlord).slice(0, 3);
   return (
     <Container>
-      <Box pb={5}>
-        <Box pb={2}>
-          <Typography variant="h5" align="left" className={landlordTitle}>
-            Landlords
-          </Typography>
-        </Box>
-        <Grid container spacing={2}>
-          {landlordSearchResults.map((landlordSearchResult) => (
-            <Grid item xs={4} md={4}>
-              <LandlordCard landlordData={landlordSearchResult} />
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
+      <Typography variant="h5" align="left" className={landlordTitle}>
+        Landlords
+      </Typography>
     </Container>
   );
 };
