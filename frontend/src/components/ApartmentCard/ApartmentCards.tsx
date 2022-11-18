9ce036bc533fb3563c3af112f91b34f87251af49
thuypham03 <sophia.phamthuy@gmail.com>
2022-11-18 15:38:45 -0500

    add inf-scroll in location page and notify end of scroll

--- frontend/src/components/ApartmentCard/ApartmentCards.tsx
+++ frontend/src/components/ApartmentCard/ApartmentCards.tsx
@@ -16,6 +16,7 @@ const useStyles = makeStyles({
 });
 
 const ApartmentCards = ({ data }: Props): ReactElement => {
+  console.log(data);
   const { boundingBox } = useStyles();
 
   return (
--- frontend/src/components/ApartmentCard/ScrollingCards.tsx
+++ frontend/src/components/ApartmentCard/ScrollingCards.tsx
@@ -4,6 +4,7 @@ import { loadingLength } from '../../constants/HomeConsts';
 import { CardData } from '../../App';
 import { get } from '../../utils/call';
 import { makeStyles, Typography } from '@material-ui/core';
+import { da } from 'date-fns/locale';
 
 const useStyles = makeStyles({
   loadingMsg: {
@@ -15,16 +16,23 @@ type Props = {
   API: string;
 };
 
+type returnData = {
+  buildingData: CardData[];
+  isEnded: boolean;
+};
+
 const ScrollingCards = ({ API }: Props): ReactElement => {
   const [dataSize, setDataSize] = useState(loadingLength);
-  const [data, setData] = useState<CardData[]>([]);
+  const [data, setData] = useState<returnData>({ buildingData: [], isEnded: false });
   const { loadingMsg } = useStyles();
 
   useEffect(() => {
-    get<CardData[]>(API + `${dataSize}`, {
+    get<returnData>(API + `${dataSize}`, {
       callback: setData,
     });
   }, [dataSize, API]);
+
+  console.log(data.buildingData);
 
   window.onscroll = function (ev: Event) {
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
@@ -34,8 +42,10 @@ const ScrollingCards = ({ API }: Props): ReactElement => {
 
   return (
     <>
-      <ApartmentCards data={data} />
-      <Typography className={loadingMsg}>Loading more apartments...</Typography>
+      <ApartmentCards data={data.buildingData} />
+      <Typography className={loadingMsg}>
+        {!data.isEnded ? 'Loading more apartments...' : "There's no more apartments!"}
+      </Typography>
     </>
   );
 };
