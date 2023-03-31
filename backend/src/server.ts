d9cb22c9d5b71c244162cce09c88cb1bdb8fe508
Thuy Pham <62644357+thuypham03@users.noreply.github.com>
2023-03-31 16:04:58 -0400

    Test Heroku (#266)
    * testing
    
    * add server
    
    * add server
    
    * add server
    
    * add server
    
    * add server
    
    * add server
    
    * remove comment
    
    * add /
    
    * add /

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,16 @@
+import express from 'express';
+import path from 'path';
 import app from './app';
 
 const port = process.env.PORT || 8080;
 
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../../../../frontend/build')));
+
+// Anything that doesn't match above (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../../../../frontend/build/index.html`));
+});
