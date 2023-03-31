a0f4a78e29e73156961054f4574ced0118ebe303
Thuy Pham <62644357+thuypham03@users.noreply.github.com>
2023-03-30 21:30:25 -0400

    heroku fix (#265)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,6 @@
-import path from 'path';
 import app from './app';
 
-function setup() {
-  const port = process.env.PORT || 8080;
-  app.get('*', (_, response) =>
-    response.sendFile(path.join(__dirname, '../../frontend/build/index.html'))
-  );
+const port = process.env.PORT || 8080;
 
-  // eslint-disable-next-line no-console
-  app.listen(port, () => console.log(`Listening on port ${port}...`));
-}
-
-setup();
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`Listening on port ${port}...`));
