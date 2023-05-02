163a94fe1d12d592e1dc7e164ad78745c891582b
Jessica Han <JH2228@cornell.edu>
2023-05-02 19:21:57 -0400

    fixed search bar query bug

--- frontend/src/components/Home/Autocomplete.tsx
+++ frontend/src/components/Home/Autocomplete.tsx
@@ -82,6 +82,7 @@ export default function Autocomplete() {
     } else if (event.key === 'Enter') {
       setFocus(true);
       history.push(`/search?q=${query}`);
+      setQuery('');
       setOpen(false);
     }
   }
