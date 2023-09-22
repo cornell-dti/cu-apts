0539482fc44c7b11ee020809459177c4b955ae48
Thuy Pham <tpp38@cornell.edu>
2023-09-22 17:31:32 -0400

    add admin permission

--- frontend/src/constants/HomeConsts.ts
+++ frontend/src/constants/HomeConsts.ts
@@ -1,14 +1,14 @@
 export const loadingLength = 5;
 export const admins: string[] = [
-  'cri23@cornell.edu',
-  'asl256@cornell.edu',
-  'am2749@cornell.edu',
-  'dj263@cornell.edu',
-  'feb47@cornell.edu',
-  'gs622@cornell.edu',
-  'jh2228@cornell.edu',
-  'kjc249@cornell.edu',
-  'tp267@cornell.edu',
-  'sy398@cornell.edu',
-  'tpp38@cornell.edu',
+  'cri23@cornell.edu', // Cyrus
+  'asl256@cornell.edu', // Ankit
+  'gs622@cornell.edu', // Grace
+  'jh2228@cornell.edu', // Jessica
+  'kjc249@cornell.edu', // Katherine
+  'sy398@cornell.edu', // Sarah
+  'tpp38@cornell.edu', // Sophia
+  'cl874@cornell.edu', // Clara
+  'km653@cornell.edu', // Kevin
+  'mml267@cornell.edu', // Miranda
+  'to98@cornell.edu', // Tina
 ];
