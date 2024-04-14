4f54859a04625a9c4549c1f9c50eb405f3c471b9
Kea-Roy Ong <146872846+kea-roy@users.noreply.github.com>
2024-04-14 01:52:07 -0400

    Revamped FAQ Page

--- frontend/src/components/FAQ/CollapsibleQuestion.tsx
+++ frontend/src/components/FAQ/CollapsibleQuestion.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import {
   Accordion,
@@ -6,31 +6,75 @@ import {
   AccordionDetails,
   Typography,
   Grid,
-  styled,
+  makeStyles,
 } from '@material-ui/core';
+import { colors } from '../../colors';
 
 type Props = {
   readonly question: string;
   readonly answer: string;
 };
 
-const StyledAccordion = styled(Accordion)({
-  backgroundColor: 'rgba(0, 0, 0, .03)',
-});
-
+/**
+ * CollapsableQuestion Component
+ *
+ * Displays a question and answer in a collapsable format. Formatted for the FAQ page.
+ *
+ * @param answer - the answer to the question
+ * @param question - the question to be displayed
+ * @returns ReactElement
+ *
+ */
 export default function CollapsableQuestion({ answer, question }: Props): ReactElement {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 600);
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const useStyles = makeStyles(() => ({
+    questionTitle: {
+      fontSize: isMobile ? '10px' : '22px',
+      fontWeight: 600,
+      lineHeight: isMobile ? '14px' : '32px',
+      color: colors.red1,
+    },
+    answerBlurb: {
+      fontSize: isMobile ? '10px' : '18px',
+      fontWeight: 400,
+      lineHeight: isMobile ? '14px' : '28px',
+      color: colors.black,
+    },
+    questionContainer: {
+      borderRadius: '8px',
+      border: '1px solid' + colors.gray5,
+      background: colors.white,
+      marginBottom: isMobile ? ('8px!important' as '8px') : ('16px!important' as '16px'),
+      padding: isMobile ? '0px' : '12px',
+    },
+  }));
+  const { questionTitle, answerBlurb, questionContainer } = useStyles();
   return (
     <Grid item>
-      <StyledAccordion variant="outlined" square>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography variant="body1">{question}</Typography>
+      <Accordion variant="outlined" square className={questionContainer}>
+        <AccordionSummary
+          expandIcon={
+            <ExpandMoreIcon fontSize={isMobile ? 'medium' : 'large'} htmlColor={colors.red1} />
+          }
+        >
+          <Typography variant="body1" className={questionTitle}>
+            {question}
+          </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography gutterBottom variant="body2">
+          <Typography gutterBottom variant="body2" className={answerBlurb}>
             {answer}
           </Typography>
         </AccordionDetails>
-      </StyledAccordion>
+      </Accordion>
     </Grid>
   );
 }
